ef1924a977d700723f2af230cce9473bcd0a2fa6
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-18 00:32:19 +0800

    fix data

--- make_data.sh
+++ make_data.sh
@@ -1,5 +1,5 @@
 python processing_data.py \
     --src-domain clean --tgt-domain noisy \
     --path /home/stud_vantuan/share_with_150/cache/helicopter_1h_30m \
-    --dest-path /home/stud_vantuan/data/data_pickle_gan \
+    --dest-path /home/stud_vantuan/data/data_pickle_gan/helicopter_1h_30m \
     --split train --stage train --threads 128 \
