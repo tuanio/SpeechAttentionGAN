52b42e1737a25716b5b72b2b775a99408fd5f32d
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-17 18:55:05 +0800

    update this

--- model/attn_gan.py
+++ model/attn_gan.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import lightning as L
 from torch import nn, Tensor
+import torchaudio
 import torchaudio.transforms as T
 from itertools import chain
 from functools import reduce
