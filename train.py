5803f8fcb3be29d326de6023c6559d30da6c10a5
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-17 21:37:24 +0800

    revert main back

--- train.py
+++ train.py
@@ -9,36 +9,36 @@ import lightning.pytorch as L
 @hydra.main(version_base=None, config_path="conf", config_name="default_attn_gan")
 def main(cfg: DictConfig):
 
-    x = torch.rand(4, 1, 129, 128)
-    m = torch.rand(4, 1, 129, 128)
+    # x = torch.rand(4, 1, 129, 128)
+    # m = torch.rand(4, 1, 129, 128)
 
-    gen = AttentionGuideGenerator(**cfg.model.generator)
+    # gen = AttentionGuideGenerator(**cfg.model.generator)
 
-    o = gen(x, m)
+    # o = gen(x, m)
 
-    print(o.shape)
+    # print(o.shape)
 
-    # dm = SpeechDataModule(cfg.dm)
+    dm = SpeechDataModule(cfg.dm)
 
-    # total_steps = len(dm.train_dataloader()) * cfg.trainer.max_epochs
+    total_steps = len(dm.train_dataloader()) * cfg.trainer.max_epochs
 
-    # model = MagnitudeAttentionGAN(
-    #     cfg.model, total_steps=total_steps, istft_params=cfg.feature.istft_params
-    # )
+    model = MagnitudeAttentionGAN(
+        cfg.model, total_steps=total_steps, istft_params=cfg.feature.istft_params
+    )
 
-    # logger = None
-    # if cfg.logger.wandb.have:
-    #     logger = L.loggers.wandb.WandbLogger(**cfg.logger.wandb.params)
+    logger = None
+    if cfg.logger.wandb.have:
+        logger = L.loggers.wandb.WandbLogger(**cfg.logger.wandb.params)
 
-    # trainer = L.Trainer(logger=logger, **cfg.trainer)
+    trainer = L.Trainer(logger=logger, **cfg.trainer)
 
-    # if cfg.exp.train:
-    #     print("Start training...")
-    #     trainer.fit(model, datamodule=dm)
+    if cfg.exp.train:
+        print("Start training...")
+        trainer.fit(model, datamodule=dm)
 
-    # if cfg.exp.test:
-    #     print("Start testing...")
-    #     trainer.test(model, datamodule=dm)
+    if cfg.exp.test:
+        print("Start testing...")
+        trainer.test(model, datamodule=dm)
 
 
 if __name__ == "__main__":
