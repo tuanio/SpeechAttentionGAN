db20d156dbf7c67242b764d634b52e342e9e6be7
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-18 00:49:59 +0800

    remove ()

--- model/downsample.py
+++ model/downsample.py
@@ -26,7 +26,7 @@ class SimpleDownsample(nn.Module):
         paddings=[0, 1, 1],
         dilations=[1, 1, 1],
         norm_layer=nn.InstanceNorm2d,  # can be instance norm
-        activation=nn.SiLU, # can be relu
+        activation=nn.ReLU, # can be relu
         **kwargs,  
     ):
         super().__init__()
