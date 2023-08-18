9a22512242121798dec79d8a10b1f1fc2716c4fd
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-18 20:28:31 +0800

    re do for mekdata

--- make_data.sh
+++ make_data.sh
@@ -1,5 +1,7 @@
-python processing_data.py \
-    --src-domain clean --tgt-domain noisy \
-    --path /home/stud_vantuan/share_with_150/cache/helicopter_1h_30m \
-    --dest-path /home/stud_vantuan/data/data_pickle_gan/helicopter_1h_30m \
-    --split train --stage train --threads 128 \
+for subset in train test valid:
+    echo $subset
+    python processing_data.py \
+        --src-domain clean --tgt-domain noisy \
+        --path /home/stud_vantuan/share_with_150/cache/helicopter_1h_30m \
+        --dest-path /home/stud_vantuan/data/data_pickle_gan/helicopter_1h_30m \
+        --split $subset --stage $subset --threads 128 \
