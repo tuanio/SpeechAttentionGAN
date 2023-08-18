2aeb7c622b9f03cdf4ec522cf41dcf1a8cdb07d6
Tuan Nguyen <nvatuan3@gmail.com>
2023-08-18 20:29:03 +0800

    done

--- make_data.sh
+++ make_data.sh
@@ -1,7 +1,8 @@
-for subset in train test valid:
+for subset in train test valid; do
     echo $subset
     python processing_data.py \
         --src-domain clean --tgt-domain noisy \
         --path /home/stud_vantuan/share_with_150/cache/helicopter_1h_30m \
         --dest-path /home/stud_vantuan/data/data_pickle_gan/helicopter_1h_30m \
         --split $subset --stage $subset --threads 128 \
+done
